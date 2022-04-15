4c243073b56df9571a82d90bd805cc94a5edb346
Rosivan Cardoso Ferreira <rosivancardoso767@gmail.com>
2022-04-15 00:38:25 -0400

    feat: added styled-components and settins eslint and pretierr

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+export function App() {
+  return <Text>Opa</Text>;
+}
---
+++ src/screens/Login/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+export function Login() {
+  return <Text>lOGIN</Text>;
+}
