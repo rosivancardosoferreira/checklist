0c7a36819acc0cb6c5403e9ec5b4e756c2ca5a05
Rosivan Cardoso Ferreira <rosivancardoso767@gmail.com>
2022-04-15 00:49:14 -0400

    feat: type to styled-component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {Text} from "react-native";
 
-export function Login() {
-  return <Text>lOGIN</Text>;
+export function App() {
+  return <Text>Opa</Text>;
 }
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,1 +1,11 @@
 import {DefaultTheme} from "styled-components";
+
+const theme: DefaultTheme = {
+  colors: {
+    primary: {
+      red: "red",
+    },
+  },
+};
+
+export default theme;
